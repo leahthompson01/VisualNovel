66678f6d29c9220c93557a6f430aa249b4c8e9a6
leahthompson01 <leahmthompson03@gmail.com>
2023-03-04 02:23:54 -0500

    Updated MenuScreen code with broken background Image

--- core/src/com/leahtcodes/plank/views/MenuScreen.java
+++ core/src/com/leahtcodes/plank/views/MenuScreen.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.leahtcodes.plank.Plank;
 import org.w3c.dom.Text;
@@ -17,25 +20,39 @@ public class MenuScreen implements Screen{
 
     private Plank parent;
     private Stage stage;
-    private Texture background;
+    private Skin skin;
     public MenuScreen(Plank plank){
         parent = plank;
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
         stage.act(Math.min(Gdx.graphics.getDeltaTime(),1/30f));
+        Texture image = new  Texture (Gdx.files.internal("Spring_Park1.png"));
+        TextureRegion region = new TextureRegion(image, 20, 20,  100, 100);
+//        Image img = new Image(drawable);
+        stage.getBatch().begin();
+        stage.getBatch().draw(region,400, 400);
+        stage.getBatch().end();
+
 
         stage.draw();
         Table table = new Table();
         table.setFillParent(true);
         table.setDebug(true);
         stage.addActor(table);
-        Skin skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+        skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+
+
         TextButton newGame = new TextButton("New Game", skin);
         TextButton exit = new TextButton("Exit Game", skin);
+        TextButton loadGame = new TextButton("Load Game", skin);
+        TextButton gallery = new TextButton("Gallery", skin);
 
-        table.add(newGame).fillX().uniformX();
-        table.row().pad(10,0,10,0);
-        table.add(exit).fillX().uniformX();
+        table.add(newGame).padTop(300).bottom().space(10);
+        table.add(loadGame).padTop(300).bottom().space(10);
+        table.add(gallery).padTop(300).bottom().space(10);
+        table.add(exit).padTop(300).bottom().space(10);
+
+
 
     }
     @Override
@@ -72,6 +89,9 @@ public class MenuScreen implements Screen{
     @Override
     public void dispose() {
         // TODO Auto-generated method stub
+        skin.dispose();
+        stage.dispose();
+
     }
 
 }
